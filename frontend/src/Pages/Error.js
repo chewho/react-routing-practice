3c5a0c8a3f5f9cf2ebf75d78df1c0c88744ce4e9
chewho <molocrips@gmail.com>
2023-11-24 20:16:57 +0100

    feat: add throwing response to extract error data

--- frontend/src/Pages/Error.js
+++ frontend/src/Pages/Error.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
+
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
 
 export const ErrorPage = () => {
-  return <h1>An error occured!</h1>;
+  const error = useRouteError();
+
+  let title = "An error occured!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resourse or page.";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 };
